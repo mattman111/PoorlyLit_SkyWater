f00f84dc19637e6de3ed6bdbfa70517c3f3e10ad
mattman111 <12969498+mattman111@users.noreply.github.com>
2026-07-02 12:30:37 -0400

    camera test

---
+++ Assets/Scripts/Camera/CameraTriggerZones.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+
+public class CameraTriggerZones : MonoBehaviour
+{
+    //Need to somehow get the current camera? Gonna hard code it. 
+    [SerializeField] private CinemachineCamera _otherCamera;
+    [SerializeField] private CinemachineCamera _triggerCamera;
+    [SerializeField] private GameObject _player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _triggerCamera.enabled = true;
+            _otherCamera.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _triggerCamera.enabled = false;
+            _otherCamera.enabled = true;
+        }
+    }
+}
+
